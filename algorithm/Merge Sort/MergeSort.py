adb12715e4247fe2ccf739a81e1c0b75f95f2146
Darren Liang <darrrenliang@gmail.com>
2022-05-25 20:26:58 +0800

    Update copyright

--- algorithm/Merge Sort/MergeSort.py
+++ algorithm/Merge Sort/MergeSort.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 """
-:Copyright: © 2022 Advanced Control Systems, Inc. All Rights Reserved.
+:Copyright: © 2022 Darren Liang, Inc. All Rights Reserved.
 
 @Author: Darren Liang
 @Date  : 2022-03-06
